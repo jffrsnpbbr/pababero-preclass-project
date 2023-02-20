34ba386e9183d3a16cbd503d779d97f4872ec216
jffrsnpbbr <jefferson.pababero@gmail.com>
2023-02-20 15:37:35 +0800

    activity 4: create a function that takes an array of ages and return an array of unique ages

--- activities/activity_4.rb
+++ activities/activity_4.rb
@@ -2,3 +2,25 @@
 #  Write a function that takes an array of ages.
 #  The function will return an Array of ages that you could only see one time.
 #  Print on console the result.
+
+def unique_ages(ages)
+  unique = []
+
+  ages.each do |current|
+    counter = 0
+
+    ages.each do |age|
+      if current == age
+        counter += 1
+      end
+    end
+
+    if counter == 1
+      unique.push(current)
+    end
+  end
+
+  return unique
+end
+
+p unique_ages([31,21,19,24,31,19])
