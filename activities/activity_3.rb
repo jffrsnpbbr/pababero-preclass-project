aae240e3f5d10e5b23a62aa923aedbe143a2b6c2
jffrsnpbbr <jefferson.pababero@gmail.com>
2023-02-20 15:12:37 +0800

    activity 3: create a function that returns an array of ages differences between each numbers

--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,15 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+
+def age_differences(ages)
+  differences = []
+  ages.each_with_index do |age, index|
+    if (index < ages.length-1)
+      differences.push(ages[index] - ages[index+1])
+    end
+  end
+  return differences
+end
+
+print age_differences([-5,-100,15,20,25])
